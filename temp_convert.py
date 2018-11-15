1997d12a5ba2ce4f020f71ffc67cb09fc6065e31
chongchen <cc.chong.chen@gmail.com>
2018-11-15 21:23:43 +0800

    add exercise files

---
+++ temp_convert.py
@@ -0,0 +1,9 @@
+temp_str = input("请输入带符号的温度值:")
+if temp_str[-1] in ['F', 'f']:
+    c = (eval(temp_str[0:-1]) - 32) / 1.8
+    print("转换后的温度是:{:.2f}C".format(c))
+elif temp_str[-1] in ['C', 'c']:
+    f = 1.8*eval(temp_str[0:-1]) + 32
+    print("转换后的温度是:{:.2f}F".format(f))
+else:
+    print("输入有误:")
